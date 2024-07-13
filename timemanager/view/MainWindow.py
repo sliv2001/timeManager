11a3e4b51f38c9df589068bed5b5742a63babc4a
Ivan Sladkov <ivan.sladkov@syntacore.com>
2024-07-13 10:05:59 +0300

    [master] Typo fix

--- timemanager/view/MainWindow.py
+++ timemanager/view/MainWindow.py
@@ -26,8 +26,10 @@ class MainWindow(QMainWindow):
     newItemButton.clicked.connect(slot=self.newItemButton_clicked)
     self.ui.buttonBox.addButton(newItemButton, QDialogButtonBox.ButtonRole.ActionRole)
     self.ui.listWidget.itemChanged.connect(slot=self.item_checked)
-    self.ui.removeItem.triggered.connect(slot=self.deleteTriggered)
-    self.ui.listWidget.addAction(self.ui.removeItem)
+
+    self.ui.menu = QMenu(self.ui.listWidget)
+    self.ui.menu.addAction("Удалить", self, "deleteTriggered")
+
 
     self.update()
 
