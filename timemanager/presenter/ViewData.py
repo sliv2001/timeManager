a6b70faa5867ac9dfe3bc7eeeeb3020d0973874c
Ivan Sladkov <ivan.sladkov@syntacore.com>
2024-07-15 23:37:35 +0300

    [md_comments] Added comment to ViewData

--- timemanager/presenter/ViewData.py
+++ timemanager/presenter/ViewData.py
@@ -2,13 +2,14 @@ from .Statuses import Statuses
 
 class ViewData():
 
-  def __init__(self, itemName, itemPK, status, dateTime, elapsedTime, timeout = 24*3600) -> None:
+  def __init__(self, itemPK, itemName = None, status = None, dateTime = None, elapsedTime = None, timeout = None, comment = None) -> None:
     self.itemName = itemName
     self.itemPK = itemPK
     self.timeout = timeout
     self.status = status
     self.dateTime = dateTime
     self.elapsedTime = elapsedTime
+    self.comment = comment
 
   def done(self):
     return self.status == Statuses.Done
