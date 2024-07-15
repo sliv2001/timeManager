376f8e275909941f747e8c418967ee2738d999a5
Ivan Sladkov <ivan.sladkov@syntacore.com>
2024-07-15 23:36:25 +0300

    [md_comments] Added comment to ListItem

--- timemanager/view/listItem.py
+++ timemanager/view/listItem.py
@@ -2,6 +2,8 @@ from PySide6.QtWidgets import QListWidgetItem
 
 class ListItem(QListWidgetItem):
   itemPK: int
-  def __init__(self, item, itemPK) -> None:
+  comment: str
+  def __init__(self, item, itemPK, comment = "") -> None:
     self.itemPK = itemPK
+    self.comment = comment
     return super().__init__(item)
