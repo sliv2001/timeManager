b844d7ac4f711413641ad82446f4792c60351dbb
Ivan Sladkov <ivan.sladkov@yandex.ru>
2024-07-20 19:21:25 +0300

    [verboseView] Added action for showing verboseInfo

--- timemanager/view/Ui_mainWindow.py
+++ timemanager/view/Ui_mainWindow.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'mainWindowwheeZV.ui'
+## Form generated from reading UI file 'mainWindowVqifaa.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.7.2
 ##
@@ -40,6 +40,11 @@ class Ui_MainWindow(object):
         icon1 = QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DocumentNew))
         self.addItem.setIcon(icon1)
         self.addItem.setMenuRole(QAction.MenuRole.NoRole)
+        self.verboseItem = QAction(MainWindow)
+        self.verboseItem.setObjectName(u"verboseItem")
+        icon2 = QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DocumentProperties))
+        self.verboseItem.setIcon(icon2)
+        self.verboseItem.setMenuRole(QAction.MenuRole.NoRole)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout = QGridLayout(self.centralwidget)
@@ -47,7 +52,7 @@ class Ui_MainWindow(object):
         self.listWidget = QListWidget(self.centralwidget)
         self.listWidget.setObjectName(u"listWidget")
         self.listWidget.setContextMenuPolicy(Qt.ContextMenuPolicy.ActionsContextMenu)
-        self.listWidget.setSelectionMode(QAbstractItemView.SelectionMode.MultiSelection)
+        self.listWidget.setSelectionMode(QAbstractItemView.SelectionMode.ExtendedSelection)
 
         self.gridLayout.addWidget(self.listWidget, 1, 0, 1, 1)
 
@@ -56,11 +61,11 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
 
-        self.itemVerbose = QGroupBox(self.centralwidget)
-        self.itemVerbose.setObjectName(u"itemVerbose")
-        self.verticalLayout = QVBoxLayout(self.itemVerbose)
+        self.itemVerboseGroupBox = QGroupBox(self.centralwidget)
+        self.itemVerboseGroupBox.setObjectName(u"itemVerboseGroupBox")
+        self.verticalLayout = QVBoxLayout(self.itemVerboseGroupBox)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.tabWidget = QTabWidget(self.itemVerbose)
+        self.tabWidget = QTabWidget(self.itemVerboseGroupBox)
         self.tabWidget.setObjectName(u"tabWidget")
         self.markdown = QWidget()
         self.markdown.setObjectName(u"markdown")
@@ -87,7 +92,7 @@ class Ui_MainWindow(object):
         self.verticalLayout.addWidget(self.tabWidget)
 
 
-        self.gridLayout.addWidget(self.itemVerbose, 2, 0, 1, 1)
+        self.gridLayout.addWidget(self.itemVerboseGroupBox, 2, 0, 1, 1)
 
         self.buttonBox = QDialogButtonBox(self.centralwidget)
         self.buttonBox.setObjectName(u"buttonBox")
@@ -128,8 +133,15 @@ class Ui_MainWindow(object):
 #if QT_CONFIG(shortcut)
         self.addItem.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+N", None))
 #endif // QT_CONFIG(shortcut)
+        self.verboseItem.setText(QCoreApplication.translate("MainWindow", u"\u041f\u043e\u0434\u0440\u043e\u0431\u043d\u043e", None))
+#if QT_CONFIG(tooltip)
+        self.verboseItem.setToolTip(QCoreApplication.translate("MainWindow", u"\u041f\u043e\u043b\u0443\u0447\u0438\u0442\u044c \u043f\u043e\u0434\u0440\u043e\u0431\u043d\u0443\u044e \u0438\u043d\u0444\u043e\u0440\u043c\u0430\u0446\u0438\u044e \u043e\u0431 \u044d\u043b\u0435\u043c\u0435\u043d\u0442\u0435 \u0438 \u0438\u0437\u043c\u0435\u043d\u0438\u0442\u044c \u0441\u0432\u043e\u0439\u0441\u0442\u0432\u0430", None))
+#endif // QT_CONFIG(tooltip)
+#if QT_CONFIG(shortcut)
+        self.verboseItem.setShortcut(QCoreApplication.translate("MainWindow", u"Return", None))
+#endif // QT_CONFIG(shortcut)
         self.label.setText(QCoreApplication.translate("MainWindow", u"\u042d\u0442\u043e \u0441\u043f\u0438\u0441\u043e\u043a \u0434\u0435\u043b, \u043a\u043e\u0442\u043e\u0440\u044b\u0435 \u043d\u0443\u0436\u043d\u043e \u0432\u044b\u043f\u043e\u043b\u043d\u0438\u0442\u044c \u0432 \u0442\u0435\u0447\u0435\u043d\u0438\u0435 \u0434\u043d\u044f:", None))
-        self.itemVerbose.setTitle(QCoreApplication.translate("MainWindow", u"GroupBox", None))
+        self.itemVerboseGroupBox.setTitle(QCoreApplication.translate("MainWindow", u"GroupBox", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.text), QCoreApplication.translate("MainWindow", u"\u0424\u043e\u0440\u043c\u0430\u0442\u0438\u0440\u043e\u0432\u0430\u043d\u043d\u044b\u0439 \u0442\u0435\u043a\u0441\u0442", None))
     # retranslateUi
 
