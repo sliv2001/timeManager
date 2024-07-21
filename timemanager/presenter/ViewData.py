0703448bf1fe55b1aefd1438523231c86f6cf4c7
Ivan Sladkov <ivan.sladkov@yandex.ru>
2024-07-21 18:57:10 +0300

    [verboseView] Added constructor overload for ViewData

--- timemanager/presenter/ViewData.py
+++ timemanager/presenter/ViewData.py
@@ -1,4 +1,6 @@
-from .Statuses import Statuses
+from pony import orm
+from timemanager.model.model import Fulfill, Items, Statuses
+from .Statuses import Statuses as PresenterStatuses
 
 class ViewData():
 
@@ -11,5 +13,22 @@ class ViewData():
     self.elapsedTime = elapsedTime
     self.comment = comment
 
+  @orm.db_session
+  def __init__(self, item: Items = None, fulfill: Fulfill = None):
+    if item is None and fulfill is None:
+      raise RuntimeError('Cannot create View Data with empty initializers')
+    if item is not None:
+      self.itemName = item.name
+      self.itemPK = item.pk
+      self.timeout = item.timeout
+      statusEntry = Statuses[item.status]
+      self.status = statusEntry.name
+      self.comment = item.comment
+      self.dateTime = None
+      self.elapsedTime = None
+    if fulfill is not None:
+      self.dateTime = fulfill.dateTime
+      self.elapsedTime = fulfill.elapsedTime
+
   def done(self):
-    return self.status == Statuses.Done
+    return self.status == PresenterStatuses.Done
