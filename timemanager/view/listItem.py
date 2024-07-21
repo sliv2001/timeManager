4fca7fd7927f562294014961fbaff60aceccf51d
Ivan Sladkov <ivan.sladkov@yandex.ru>
2024-07-21 17:56:24 +0300

    [verboseView] Removed comment from listItem

--- timemanager/view/listItem.py
+++ timemanager/view/listItem.py
@@ -2,8 +2,6 @@ from PySide6.QtWidgets import QListWidgetItem
 
 class ListItem(QListWidgetItem):
   itemPK: int
-  comment: str
-  def __init__(self, item, itemPK, comment = "") -> None:
+  def __init__(self, item, itemPK):
     self.itemPK = itemPK
-    self.comment = comment
     return super().__init__(item)
