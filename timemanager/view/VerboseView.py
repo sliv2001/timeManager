dbd237ea47f3b1dffe5029782e365b5fb9a0ac55
Ivan Sladkov <ivan.sladkov@syntacore.com>
2024-07-24 14:38:47 +0300

    [verboseView] Fixed saving and added formatting to MD

--- timemanager/view/VerboseView.py
+++ timemanager/view/VerboseView.py
@@ -18,6 +18,7 @@ class VerboseView:
     self.ui.itemVerboseButtonBox.button(QDialogButtonBox.StandardButton.Apply).clicked.connect(self.saveAndHide)
     self.ui.itemVerboseButtonBox.button(QDialogButtonBox.StandardButton.Close).setText("Скрыть")
     self.ui.itemVerboseButtonBox.button(QDialogButtonBox.StandardButton.Close).clicked.connect(self.hide)
+    self.ui.tabWidget.currentChanged.connect(slot=self.switchTextFormat)
     self.hide()
 
   @Slot()
@@ -50,9 +51,25 @@ class VerboseView:
 
   @Slot()
   def save(self):
-    self.verboseText = self.ui.itemVerboseTextEdit.toPlainText()
+    if (self.ui.tabWidget.currentIndex() == 0):
+      self.verboseText = self.ui.itemVerboseTextEdit.toPlainText()
+    elif (self.ui.tabWidget.currentIndex() == 1):
+      self.verboseText = self.ui.itemVerboseTextView.toMarkdown()
+    else:
+      raise RuntimeError('Unexpected index of tab in TabWidget!')
     self.presenter.UpdateItem(ViewData(self.itemPK, comment=self.verboseText))
 
   @Slot()
   def hide(self):
     self.ui.itemVerboseGroupBox.hide()
+
+  @Slot()
+  def switchTextFormat(self, newTab: int):
+    if newTab == 0:
+      # Switch to MD:
+      self.ui.itemVerboseTextEdit.setPlainText(self.ui.itemVerboseTextView.toMarkdown())
+    elif newTab == 1:
+      # Switch to formatted text
+      self.ui.itemVerboseTextView.setMarkdown(self.ui.itemVerboseTextEdit.toPlainText())
+    else:
+      raise RuntimeError('New tab was chosen incorrectly!')
