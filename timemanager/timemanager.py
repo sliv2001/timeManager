0c964072af076f84485f7581de8e41c1267076f3
Ivan Sladkov <ivan.sladkov@syntacore.com>
2024-11-23 17:17:20 +0300

    [feat/config] Added custom application

--- timemanager/timemanager.py
+++ timemanager/timemanager.py
@@ -1,10 +1,13 @@
 import sys
+from PySide6.QtCore import QSettings
 from PySide6.QtWidgets import QApplication
 from timemanager.view.MainWindow import MainWindow
 
+from timemanager.application import Application
 
 def main():
-    app = QApplication(sys.argv)
+    settings = QSettings('ISTech', 'TimeManager')
+    app = Application(settings, sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
