de9442ae917c6acba8eea0df669301d70767abc8
Ivan Sladkov <ivan.sladkov@syntacore.com>
2024-12-07 21:54:26 +0300

    [feat/plugins] Move settings

--- timemanager/application.py
+++ timemanager/application.py
@@ -1,6 +1,10 @@
 from PySide6.QtWidgets import QApplication
+from timemanager.utils.settings import Settings
+from timemanager.plugins.pluginHandler import pluginHandler
 
 class Application(QApplication):
-  def __init__(self, settings, *args, **kwargs):
+  def __init__(self, *args, **kwargs):
+    settings = Settings('ISTech', 'TimeManager')
+    self.pluginHandler = pluginHandler(settings)
     self.settings = settings
     super().__init__(*args, **kwargs)
--- timemanager/timemanager.py
+++ timemanager/timemanager.py
@@ -1,13 +1,11 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from timemanager.view.MainWindow import MainWindow
-from timemanager.utils.settings import Settings
 
 from timemanager.application import Application
 
 def main():
-    settings = Settings('ISTech', 'TimeManager')
-    app = Application(settings, sys.argv)
+    app = Application(sys.argv)
     window = MainWindow(app.settings)
     window.show()
     sys.exit(app.exec())
