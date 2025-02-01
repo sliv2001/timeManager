8816e0cbd15e904d1072dc1112aa617db723e67e
Ivan Sladkov <ivan.sladkov@syntacore.com>
2025-02-01 17:56:10 +0300

    [feat/plugin/coloring] Rework timers plugin

--- timemanager/plugins/timers/timers.py
+++ timemanager/plugins/timers/timers.py
@@ -1,29 +1,13 @@
 from typing import Any
-from PySide6.QtCore import QModelIndex, QPersistentModelIndex, Qt, QTimer, Signal
+from datetime import datetime
+from PySide6.QtCore import QModelIndex, QPersistentModelIndex, Qt, QTimer, Signal, Slot
 from timemanager.plugins.plugin import plugin
 from timemanager.plugins.pluginHandler import modify_func
+from timemanager.plugins.timers.timers_handler import TimersHandler
+from timemanager.presenter.Statuses import ViewStatuses
+from timemanager.presenter.ViewData import ViewData
 
 class timers(plugin):
-
-  _timers = []
-  _timer: QTimer
-
-  def _timerAdd(self, id, timeout):
-    self._timers.append((id, timeout))
-    self._timer.start()
-
-  def _1SecTick(self):
-    for timer in self._timers:
-      if timer[1] == 0:
-        self.setStateFinished(timer[0])
-        self._timers = [timer for timer in self._timers if timer[1]>0]
-        if self._timers == []:
-          self._timer.stop()
-      else:
-        timer[1] -= 1
-
-  def setStateFinished(self, id):
-    pass
 
   def requiresModelUpdate(self) -> bool:
     return False
@@ -32,23 +16,47 @@ class timers(plugin):
     pass
 
   def appUpdate(self):
-    self._timer = QTimer(self)
-    self._timer.timeout.connect(slot=self._1SecTick)
-    self._timer.setInterval(1000)
-    self.app.presenter.dataUpdated = Signal(None, name = 'dataUpdated')
-    self.app.presenter.dataUpdated.connect(self.)
+    self.timers_handler = TimersHandler(self)
+    self.app.presenter.dataChanged.connect(slot=self.dataChanged)
+    self.timers_handler.timerFinished.connect(slot=self.timerFinished)
 
-  def handleCheckClick(self):
-    raise NotImplementedError('Need to implement')
+# //////////////////////////////////////////////////////// Signals Handling /////////////////////////////////////////////////////// #
+
+  @Slot()
+  def dataChanged(self, begin: QModelIndex, end: QModelIndex, roles: list[int]):
+    if Qt.ItemDataRole.CheckStateRole in roles:
+      for row in range(begin.row(), end.row()+1): # Because if (1, 1), we need to process it
+        intID = self.app.presenter.index(row, 0).internalId()
+        done = self.app.presenter._getCache()[row].status == ViewStatuses.Done
+        if not done:
+          continue
+        timeout = self.app.presenter.GetItem(intID).timeout
+        self.startTheTimer(row, timeout)
+
+  @Slot()
+  def timerFinished(self, intID: int):
+    index = self.app.presenter.createIndex(intID, 0)
+    self.app.presenter._updateItem(ViewData(index.internalId(),
+                                            status=ViewStatuses.Done,
+                                            dateTime=datetime.now(),
+                                            elapsedTime=15*60))
+    self.app.presenter.dataChanged.emit(index, index, [Qt.ItemDataRole.CheckStateRole])
+    print('finished entry', intID)
+
+# ///////////////////////////////////////////////////////////// Timers //////////////////////////////////////////////////////////// #
+
+  def startTheTimer(self, row: int, timeout: int):
+    self.timers_handler.createEntry(row, timeout)
 
 # ///////////////////////////////////////////////////// New Presenter Features //////////////////////////////////////////////////// #
 
-  @modify_func('setData')
-  def setData(self, index: QModelIndex | QPersistentModelIndex, value: Any, role: int = ...):
-    if (role == Qt.ItemDataRole.CheckStateRole) and (value == Qt.CheckState.Checked.value):
+  # @modify_func('setData')
+  # def setData(self, index: QModelIndex | QPersistentModelIndex, value: Any, role: int = ...):
+  #   if role == Qt.ItemDataRole.CheckStateRole and value == Qt.CheckState.Checked.value:
+  #     self.dataChanged.emit(index, index, [role])
+  #     return False
+  #   return None
 
-      self._timerAdd(index.internalId(), self._getCache()[index.row()].timeout)
-
-  @modify_func('data')
-  def data(self, index: QModelIndex | QPersistentModelIndex, role: int = ...):
-    return None
+  # @modify_func('data')
+  # def data(self, index: QModelIndex | QPersistentModelIndex, role: int = ...):
+  #   return None
